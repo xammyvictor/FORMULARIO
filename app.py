44cb0a03f2d15b19ca7c4d227f334f881e638875
xammyvictor <xammyvictor@gmail.com>
2026-01-23 07:01:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ if check_auth():
                 map_mode = st.radio("Modo de Visualización:", ["Coropleta Territorial", "Hotspots de Concentración"], horizontal=True)
                 
                 try:
-                    geojson_url = "https://raw.githubusercontent.com/caticoa3/colombia_mapa/master/co_2018_MGN_DPTO_POLITICO.geojson"
+                    geojson_url = "https://raw.githubusercontent.com/caticoa3/colombia_mapa/master/co_2018_MGN_MPIO_POLITICO.geojson"
                     response = requests.get(geojson_url)
                     geojson_data = response.json()
                     
